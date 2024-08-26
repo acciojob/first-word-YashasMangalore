208e1a5d3bd65a3e7a21363267e18912af60642f
YashasMangalore <129035779+YashasMangalore@users.noreply.github.com>
2024-08-26 09:33:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+  // Trim leading and trailing spaces from the string
+  const trimmedStr = str.trim();
+  
+  // Check if the trimmed string is empty
+  if (trimmedStr === '') {
+    return '';
+  }
+  
+  // Find the index of the first space in the trimmed string
+  const spaceIndex = trimmedStr.indexOf(' ');
+  
+  // If there is no space, return the entire trimmed string
+  if (spaceIndex === -1) {
+    return trimmedStr;
+  }
+  
+  // Extract the substring up to the first space
+  return trimmedStr.substring(0, spaceIndex);
 }
 
 // Do not change the code below
